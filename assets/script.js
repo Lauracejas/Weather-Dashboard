f8414e86cc60ae39d0318b4ed6d751fd35261bc5
lauracejas <lauracejasmartin94@gmail.com>
2021-03-04 17:35:34 -0500

    fix the uV Index

--- assets/script.js
+++ assets/script.js
@@ -14,10 +14,11 @@ var listSearchCity = $("#list-Cities");
 function showList(event) {
     event.preventDefault();
     var listCities = $("#input-City").val();
-    var cityPut = $("<li>");
+    var cityPut = $("<a>").attr("class", "list-group-item list-group-item-action");
     cityPut.text(listCities);
     listSearchCity.append(cityPut);
 
+    storeCity();
 }
 
 /*********Store city name in local storage*********/
@@ -45,60 +46,70 @@ function currentWeather(city) {
             console.log(date);
             var weatherIcon = response.weather[0].icon;
             var iconURL = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-            cityName.html(response.name + "(" + date + ")" + "<img src=" + iconURL + ">");            
-            currentTemp.html("Temperature: " + response.main.temp + " &#8457 ");            
-            currentHum.html(response.main.humidity + "%");            
-            currentWind.html(response.wind.speed + "MPH");
+            cityName.html(response.name + "(" + date + ")" + "<img src=" + iconURL + ">");
+            currentTemp.html("Temperature: " + response.main.temp + " &#8457 ");
+            currentHum.html("Humidity: " + response.main.humidity + "%");
+            currentWind.html("Wind Speed: " + response.wind.speed + "MPH");
             var lat = response.coord.lat;
             var lon = response.coord.lon;
             getUV(lat, lon);
 
         })
+        forecast(city);
 }
 
- function getUV(lat, lon) {
+/******Get UV Index and the color that indicates whether the conditions are favorable, moderate, or severe*********/
+function getUV(lat, lon) {
     var uvURL = 'https://api.openweathermap.org/data/2.5/uvi?appid=' + apiKey + '&lat=' + lat + '&lon=' + lon;
     fetch(uvURL)
         .then(function (response) {
             return response.json()
         })
-        .then(function(response){
+        .then(function (response) {
             console.log(response);
-            currentUvi.html(response.value);
-            
+          
+          var valueIndex = response.value
+          //console.log(valueIndex)
+           if ( valueIndex > 10){
+            currentUvi.attr("class","badge badge-danger")
+           }
+           else if(valueIndex > 4){
+            currentUvi.attr("class","badge badge-warning")
+        } else   {
+            currentUvi.attr("class","badge badge-success")
+        }
+          
+            currentUvi.html("UV Index: " + response.value);
+
         });
- }
+}
 
 /********************5-Day Forecast********************************/
-// function forecast(city) {
-//     var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey + '&units=imperial';
-//     fetch(forecastURL)
-//         .then(function (response) {
-//             // console.log(response);
-//             $("#forecast-cards").html('<h5 class="card-title">5-Day Forecast</h5>');
-//             for (var i = 0; i < 5; i++) {
-
-//                 var col = $("<div>").addClass("row col-sm-3");
-//                 var card = $("<div>").addClass("card bg-primary text-white ml-2 mb-2 rounded");
-//                 var body = $("<div>").addClass("card-body p-2");
-//                 var title = $("<div>").addClass("card-title").text(new Date(response.list[i].dt).toLocaleDateString());
-//                 var iconcode = response.list[i].weather[0].icon;
-//                 var iconUrl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
-//                 var iconImage = $("<div>").addClass("icone").html("<img src=" + iconUrl + ">");
-//                 var tempCard = $("<p>").addClass("card-text").html("Temp: " + response.list[i].main.temp + "&#8457");
-//                 var humidCard = $("<p>").addClass("humidity").html("Humidity: " + response.list[i].main.humidity + "%");
-
-
-//                 //card.append(body.append(title, iconImage, tempCard, humidCard))
-
-//                 col.append(card.append(body.append(title, iconImage, tempCard, humidCard)));
-//                 $("#forecast-cards").append(col);
+function forecast(city) {
+    var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey + '&units=imperial';
+    fetch(forecastURL)
+        .then(function (response) {
+            console.log(response);
+            $("#forecast-cards").html('<h5 class="card-title">5-Day Forecast</h5>');
+            for (var i = 0; i < response.list.length; i++) {
+                if (response.list[i].dt_text.indexOf("15:00:00") !== -1) {
+                    var column = $("<div>").addClass("row col-sm-3");
+                    var card = $("<div>").addClass("card bg-primary text-white ml-2 mb-2 rounded");
+                    var body = $("<div>").addClass("card-body p-2");
+                    var title = $("<div>").addClass("card-title").text(new Date(response.list[i].dt_text).toLocaleDateString());
+                    var iconcode = response.list[i].weather[0].icon;
+                    var iconUrl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+                    var iconImage = $("<div>").addClass("icone").html("<img src=" + iconUrl + ">");
+                    var tempCard = $("<p>").addClass("card-text").html("Temp: " + response.list[i].main.temp + "&#8457");
+                    var humidCard = $("<p>").addClass("humidity").html("Humidity: " + response.list[i].main.humidity + "%");
 
 
-
-//             }
-//         });
-// }
+                    column.append(card.append(body.append(title, iconImage, tempCard, humidCard)));
+                    $("#forecast-cards").append(column);
+                }
+            }
+        });
+}
 
 // Start Btn
 $("#btn-Search").click(function (event) {
@@ -106,8 +117,8 @@ $("#btn-Search").click(function (event) {
     var city = inputCity.val().trim();
     currentWeather(city);
     showList(event);
-    storeCity();
-    //forecast(city);
+    
+    
     console.log(city);
 
 });
